bd9f41d70e0f4a2172068aa4e57cfcd3c46d4b85
Cristiano <neivacristiano@yahoo.com.br>
2021-12-29 22:54:28 -0300

    feat: add  song

--- starwars.js
+++ starwars.js
@@ -1,7 +1,12 @@
-// Seu javascript aqui :)
-// Use a Star Wars API: https://swapi.dev/
-// para fazer uma requisição assíncrona e:
-//  - Pegar a lista de filmes (AJAX) e preencher no HTML
-//  - Quando um filme for clicado, exibir sua introdução
+import { play } from './music.js';
 
 const API_ENDPOINT = 'https://swapi.dev/api'
+
+const song = {
+    title: 'Star Wars',
+    artist: 'Star Wars',
+    audioUrl: './audio/tema-sw.mp3',
+    coverImageUrl: './imgs/logo.svg',
+}
+
+play(song, document.body);
